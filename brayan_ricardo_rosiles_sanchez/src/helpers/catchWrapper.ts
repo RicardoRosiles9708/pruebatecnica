5040a345aad1b178ff912fab8743ee5916f78954
Ricardo Rosiles <119921843+RicardoRosiles9708@users.noreply.github.com>
2023-09-04 16:00:54 -0600

    Delete brayan_ricardo_rosiles_sanchez directory

--- brayan_ricardo_rosiles_sanchez/src/helpers/catchWrapper.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import { Request, Response, NextFunction } from 'express';
-
-export const catchWrapper = (cb: Function) => (req: Request, res: Response, next: NextFunction) =>
-  cb(req, res, next)?.catch(next);
--- brayan_ricardo_rosiles_sanchez/src/router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { user } from './routes/user';
-import { movie } from './routes/movie';
-
-export const router = express.Router();
-
-router.use(user);
-router.use(movie);
